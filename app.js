cbc359e652a841135bad4dd16d2573295cf2d48b
Akshat Agrawal <akshatagrawal1201@gmail.com>
2023-05-26 12:25:03 +0530

    Complete vendor route

--- app.js
+++ app.js
@@ -6,21 +6,27 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 // this route will return both the hardware and the software cpes.
-app.get("/cpe", async function(req, res) {
+app.get("/cpe/:cpeVendor", async function(req, res) {
   try
   {
     const collection1 = await dbConnectHardwareCPE();
     const collection2 = await dbConnectSoftwareCPE();
-    const documents1 = await collection1.find().toArray();
-    const documents2 = await collection2.find().toArray();
-    const combinedData = {
-      hardware_CPEs: documents1,
-      software_CPEs: documents2
-    };
-    res.json(combinedData);
+    const documents1 = await collection1.find({vendor: req.params.cpeVendor}).toArray();
+    const documents2 = await collection2.find({vendor: req.params.cpeVendor}).toArray();
+    if(documents1.length === 0 && documents2.length === 0)
+    {
+      res.send("No matching CPEs found.")
+    } else {
+      const combinedData = {
+        hardware_CPEs: documents1,
+        software_CPEs: documents2
+      };
+      res.json(combinedData);
+    }
   }
   catch(error)
   {
+    console.log("Error in '/cpe/:cpeVendor' route.");
     console.log(error);
   }
 });

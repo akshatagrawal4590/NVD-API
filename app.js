553e9b7ac3d8c7bc52950ce4249830c7de3fdaf0
Akshat Agrawal <akshatagrawal1201@gmail.com>
2023-06-05 01:19:16 +0530

    complete CPE route with CVEs returning to user

--- app.js
+++ app.js
@@ -1,66 +1,126 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const {dbConnectHardwareCPE, dbConnectSoftwareCPE, dbConnectCVE} = require("./mongo_connect.js");
-
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-// this route will return both the hardware and the software cpes.
-app.get("/cpe/:cpeName/:cpeVendor?/:cpeVersion?", async function(req, res) {
+app.get("/cpe/:type/:cpeName/:cpeVendor?/:cpeVersion?", async function(req, res) {
   try
   {
+    const cpeType = req.params.type;
     const cpeVendor = req.params.cpeVendor;
     const cpeVersion = req.params.cpeVersion;
     const cpeName = req.params.cpeName;
     const collection1 = await dbConnectHardwareCPE();
     const collection2 = await dbConnectSoftwareCPE();
-    let documents1;
-    let documents2;
+    let documents1 = [];
+    let documents2 = [];
     if(cpeVendor != undefined && cpeVersion != undefined)
     {
-      documents1 = await collection1.find({vendor: cpeVendor, model: cpeName, version: cpeVersion}).toArray();
-      documents2 = await collection2.find({vendor: cpeVendor, name: cpeName, version: cpeVersion}).toArray();
+      if(cpeType == "h")
+      {
+        documents1 = await collection1.find({vendor: cpeVendor, model: cpeName, version: cpeVersion}).toArray();
+      }
+      else
+      {
+        documents2 = await collection2.find({vendor: cpeVendor, name: cpeName, version: cpeVersion}).toArray();
+      }
     }
     else if(cpeVendor == undefined && cpeVersion == undefined)
     {
-      documents1 = await collection1.find({model: cpeName}).toArray();
-      documents2 = await collection2.find({name: cpeName}).toArray();
+      if(cpeType == "h")
+      {
+        documents1 = await collection1.find({model: cpeName}).toArray();
+      }
+      else
+      {
+        documents2 = await collection2.find({name: cpeName}).toArray();
+      }
     }
     else if(cpeVendor == undefined && cpeVersion != undefined)
     {
-      documents1 = await collection1.find({version: cpeVersion, model: cpeName}).toArray();
-      documents2 = await collection2.find({version: cpeVersion, name: cpeName}).toArray();
+      if(cpeType == "h")
+      {
+        documents1 = await collection1.find({version: cpeVersion, model: cpeName}).toArray();
+      }
+      else
+      {
+        documents2 = await collection2.find({version: cpeVersion, name: cpeName}).toArray();
+      }
     }
     else
     {
-      documents1 = await collection1.find({vendor: cpeVendor, model: cpeName}).toArray();
-      documents2 = await collection2.find({vendor: cpeVendor, name: cpeName}).toArray();
+      if(cpeType == "h")
+      {
+        documents1 = await collection1.find({vendor: cpeVendor, model: cpeName}).toArray();
+      }
+      else
+      {
+        documents2 = await collection2.find({vendor: cpeVendor, name: cpeName}).toArray();
+      }
     }
-
 
     if(documents1.length == 0 && documents2.length == 0)
     {
       res.send("No matching CPEs found.")
-    } else {
-      const combinedData = {
-        hardware_CPEs: documents1,
-        software_CPEs: documents2
-      };
-      res.json(combinedData);
+    }
+    else
+    {
+      let matchingCPEDesc = [];
+      documents1.forEach(function(item) {
+        if(item.deprecated == true)
+        {
+          matchingCPEDesc.push(item.deprecatedBy[0].cpeName);
+        }
+        else
+        {
+          matchingCPEDesc.push(item.cpeName);
+        }
+      });
+      documents2.forEach(function(item) {
+        if(item.deprecated == true)
+        {
+          matchingCPEDesc.push(item.deprecatedBy[0].cpeName);
+        }
+        else
+        {
+          matchingCPEDesc.push(item.cpeName);
+        }
+      });
+      let matchingCVEs = await getCVE(matchingCPEDesc);
+      if(cpeType == "h")
+      {
+        res.json({
+          hardware_CPEs: documents1,
+          matching_CVEs: matchingCVEs
+        });
+      }
+      else
+      {
+        res.json({
+          software_CPEs: documents2,
+          matching_CVEs: matchingCVEs
+        });
+      }
     }
   }
   catch(error)
   {
-    console.log("Error in '/cpe/:cpeVendor' route.");
-    console.log(error);
+    res.write("Error in '/cpe/:type/:cpeName/:cpeVendor?/:cpeVersion?' route.");
+    res.write(error);
+    res.end();
   }
 });
 
+async function getCVE(matchingCPEDesc)
+{
+  let cveDocs = [];
+  const collection3 = await dbConnectCVE();
+  cveDocs = await collection3.find({"configurations.nodes.cpeMatch": {$elemMatch: {criteria: {$in: matchingCPEDesc}}}}).toArray();
+  return cveDocs;
+}
 
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on Port 3000.");
+app.listen(process.env.PORT_NO, function() {
+  console.log("Server started.");
 });
--- mongo_connect.js
+++ mongo_connect.js
@@ -1,24 +1,26 @@
-  const {MongoClient} = require("mongodb");
-  const url = "mongodb://0.0.0.0:27017/mdm";
-  const client = new MongoClient(url);
-  const db_name = "mdm";
-  const result = client.connect();
-  result.then(function(res) {
-    db = res.db(db_name);
-  }).catch(function(error) {
-    console.log(console.log(error));
-  });
+require("dotenv").config();
+const {MongoClient} = require("mongodb");
+const url = process.env.URL;
+const client = new MongoClient(url);
+const db_name = process.env.DB_NAME;
+let db;
+const result = client.connect();
+result.then(function(res) {
+  db = res.db(db_name);
+}).catch(function(error) {
+  console.log(console.log(error));
+});
 
 const dbConnectHardwareCPE = function() {
-  return db.collection('hardwareCPE');
+  return db.collection(process.env.COLLECTION1);
 };
 
 const dbConnectSoftwareCPE = function() {
-  return db.collection('softwareCPE');
+  return db.collection(process.env.COLLECTION2);
 };
 
 const dbConnectCVE = function() {
-  return db.collection('cve');
+  return db.collection(process.env.COLLECTION3);
 };
 
 module.exports = {
